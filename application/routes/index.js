b50f86405e062486253580258a39c9b660213637
arpitamisal <arpitamisal2003@gmail.com>
2022-12-04 17:42:56 -0800

    milestone2

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,29 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-/*router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Arpita Vivek Misal" });
-});*/
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]", css:["stylesheet.css"],
+    js:["indexscript.js"]});
+});
 
-router.get("/", function(req, res){
-  res.render("index", {title: 'Home Page'});
-  });
-  
-  router.get("/login", function(req, res){
-  res.render('login', {title: 'Login'});
-  });
-  
-  router.get("/registration", function(req, res){
-  res.render('registration', {title: 'Registration'});
-  });
-  
-  router.get("/postimage", function(req, res){
-  res.render('postimage', {title: 'Post Image'});
-  });
-  
-  router.get("/viewpost", function(req, res){
-  res.render('viewpost', {title: 'View Post'});
-  });
+router.get("/login",function(reg,res){
+  res.render('login', {css:["stylesheet.css"]});
+});
+
+router.get("/register",function(reg,res){
+  res.render('registration', {css:["stylesheet.css"], js:["script.js"]});
+});
+
+router.get("/postimage",function(reg,res){
+  res.render('postimage', {css:["stylesheet.css"]});
+});
+
+router.get("/posts/:id",function(reg,res){
+  res.render('viewpost', {css:["stylesheet.css"]});
+});
 
 module.exports = router;
-
